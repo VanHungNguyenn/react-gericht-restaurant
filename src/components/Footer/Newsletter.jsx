3b3a0f38cab5e450fe0d01fb804e71424fb33668
Van Hung <vanhungnguyenbku@gmail.com>
2022-02-10 00:30:42 +0700

    Newsletter

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from 'react'
 
-import './Newsletter.css';
+import { SubHeading } from '../../components'
+import './Newsletter.css'
 
 const Newsletter = () => (
-  <div>
-    Newsletter
-  </div>
-);
+	<div className='app__newsletter'>
+		<div className='app__newsletter-heading'>
+			<SubHeading title='Newsletter' />
+			<h1 className='headtext__cormorant'>Subscribe To Our Newsletter</h1>
+			<p className='p_opensans'>And never miss latest Updates!</p>
+		</div>
+		<div className='app__newsletter-input flex__center'>
+			<input type='email' name='' id='' placeholder='Enter your email' />
+			<button type='button' className='custom__button'>
+				Subscribe
+			</button>
+		</div>
+	</div>
+)
 
-export default Newsletter;
+export default Newsletter
