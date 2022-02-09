42585c6587416360e95d39f264f188ac14759285
VanHungNguyen <vanhungnguyenbku@gmail.com>
2022-02-09 18:25:23 +0700

    .

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-import './Footer.css';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi'
+import { FooterOverlay, Newsletter } from '../../components'
+
+import './Footer.css'
 
 const Footer = () => (
-  <div>
-    Footer
-  </div>
-);
+	<div className='app__footer section__padding'>
+		<FooterOverlay />
+		<Newsletter />
+	</div>
+)
 
-export default Footer;
+export default Footer
